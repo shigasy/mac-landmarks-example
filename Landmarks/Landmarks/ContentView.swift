45c38a41023f67026b1ef608bf01be55de354d5c
shigasy <kxx555u.u@gmail.com>
2020-09-08 16:41:04 +0900

    add: iOSアプリから作ってく

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Keita Shiga on 2020/09/08.
+//  Copyright © 2020 Keita Shiga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
