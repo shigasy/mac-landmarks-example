424968933cd45411c6382217b89e68b803509e2c
shigasy <kxx555u.u@gmail.com>
2020-09-08 15:54:15 +0900

    init

---
+++ MacLandmarks/MacLandmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Keita Shiga on 2020/09/08.
+//  Copyright © 2020 Keita Shiga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
