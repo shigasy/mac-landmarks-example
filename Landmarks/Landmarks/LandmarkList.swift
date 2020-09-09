ab7b483d85aeb91f9c049d6f7e16a3e96c67348d
shigasy <kxx555u.u@gmail.com>
2020-09-09 18:46:42 +0900

    add: Listでデータと識別できるidを指定して、動的に表示

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,25 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Keita Shiga on 2020/09/09.
+//  Copyright © 2020 Keita Shiga. All rights reserved.
+//
+
+// Swiftでは名前空間は暗黙的であり、すべてのクラス（など）は、それらが含まれるモジュール（Xcodeターゲット）によって暗黙的にスコープされます。
+// もし、名前空間が必要な場合に、importと指定すれば識別可能
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarkData, id: \.id) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
