e8c571416cdeaca478473502e4115321c16cf788
shigasy <kxx555u.u@gmail.com>
2020-09-10 17:45:05 +0900

    rename: ファイルを整理するために移動

---
+++ Landmarks/Landmarks/View/LandmarkList/LandmarkRow.swift
@@ -0,0 +1,37 @@
+//  LandmarksRow.swift
+//
+//  Created by Keita Shiga on 2020/09/08.
+//  Copyright © 2020 Keita Shiga. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .imageScale(.medium)
+                    .foregroundColor(.yellow)
+            }
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
