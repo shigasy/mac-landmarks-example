f6a386d95a9fb136687df7e8c9f7dff5cb7f6c93
shigasy <kxx555u.u@gmail.com>
2020-09-16 10:09:50 +0900

    add: LandmarkRowをまとめたListを作成。selection機能で、選択されているかのような動作が可能。十字キーで動かせる

---
+++ MacLandmarks/MacLandmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  MacLandmarks
+//
+//  Created by Keita Shiga on 2020/09/16.
+//  Copyright © 2020 Keita Shiga. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject private var userData: UserData
+    @Binding var selectedLandmark: Landmark?
+    
+    var body: some View {
+        List(selection: $selectedLandmark) {
+            ForEach(userData.landmarks) { landmark in
+                if (!self.userData.showFavoritesOnly || landmark.isFavorite) {
+                    LandmarkRow(landmark: landmark).tag(landmark)
+                }
+            }
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList(selectedLandmark: .constant(landmarkData[0]))
+            .environmentObject(UserData())
+    }
+}
